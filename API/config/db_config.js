91b87c0b54f3cc61f98c17a6a4599afb33d60c3b
Deise Kelley <daysekelley@live.com>
2018-12-27 09:47:00 -0200

    api

---
+++ API/config/db_config.js
@@ -0,0 +1,5 @@
+var mongoose = require('mongoose');
+
+var urlString = 'mongodb://localhost/APII';
+
+mongoose.connect(urlString);
--- aplicacao/config/db_config.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose');
-
-var urlString = 'mongodb://localhost/API';
-
-mongoose.connect(urlString);
